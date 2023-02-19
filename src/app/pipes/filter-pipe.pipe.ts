2dd2274fb95a34ae824fedf7126f8c80b71a2446
akshata_rn <akshatarn123@gamail.com>
2023-02-19 21:48:28 +0530

    [AkshataRn:Filter error resolved]

--- src/app/pipes/filter-pipe.pipe.ts
+++ src/app/pipes/filter-pipe.pipe.ts
@@ -5,15 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipePipe implements PipeTransform {
 
-  transform(value: any,args: any) {
+  transform(value: any,args: any): any[] {
     console.log(args)
     if(args =="All notes"){
       return value
     }else{
       args=args.toLowerCase();
     }
-    return value.filter((note:any)=>{
+    return value?.filter((note:any)=>{
       return note.title.toLowerCase().includes(args) | note.description.toLowerCase().includes(args);
     })
+    // return value.filter((note:any)=>{
+    //   return note.title.toLowerCase().includes(args) | note.description.toLowerCase().includes(args);
+    // })
   }
 }
